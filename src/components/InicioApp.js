831480ce3c47e7a793b0247d277d2c294515b884
juansesu3 <juanseu3@hotmail.com>
2021-11-26 17:11:39 -0500

    third commit facebook button

--- src/components/InicioApp.js
+++ src/components/InicioApp.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import FacebookLogin from 'react-facebook-login';
 
 const Inicio = () => {
+
+    const responseFacebook = (response) => {
+        console.log(response);
+      }
     return (
         <div>
             <h1>Inicio</h1>
+
+            <FacebookLogin
+                appId="1088597931155576"
+                autoLoad={false}
+                fields="name,email,picture"
+                // onClick={componentClicked}
+                callback={responseFacebook}
+            />
+
+            
         </div>
     )
 }
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Descargas from '../components/Descargas';
+import FormCv1 from '../components/FormCv1';
+import FormCv2 from '../components/FormCv2';
+import Home from '../components/Home';
 import Login from '../components/Login';
 import NavBar from "../components/NavBar";
+import Perfil from '../components/Perfil';
+import PlantillasCv from '../components/PlantillasCv';
+import Tutoriales from '../components/Tutoriales';
+import VistaPreviaCv from '../components/VistaPreviaCv';
 import PrivateRouter from './PrivateRouter';
 import PublicRouter from './PublicRouter';
 import RoutersApp from './RoutersApp';
+import InicioApp from '../components/InicioApp'
 
 
 const AppRouter = () => {
@@ -13,7 +22,18 @@ const AppRouter = () => {
             <BrowserRouter>
                 <NavBar />
                 <Routes>
-                    <Route path="*" element={
+                
+                <Route path="/" element={<InicioApp/>} />
+                <Route path="/home" element={<Home/>} />
+                <Route path="/perfil" element={<Perfil/>} />
+                <Route path="/descargas" element={<Descargas/>} />
+                <Route path="/formcv1" element={<FormCv1/>} />
+                <Route path="/formcv2" element={<FormCv2/>} />
+                <Route path="/plantillascv" element={<PlantillasCv/>} />
+                <Route path="/tutoriales" element={<Tutoriales/>} />
+                <Route path="/vistaprevia" element={<VistaPreviaCv/>} />
+
+                    {/* <Route path="*" element={
 
                         <PrivateRouter>
                             <RoutersApp />
@@ -27,7 +47,7 @@ const AppRouter = () => {
                             <Login />
                         </PublicRouter>
                     }
-                    />
+                    /> */}
                 </Routes>
 
             </BrowserRouter>
