aaefb631d3f7d5399577c588d641f9f7abc2d8bf
juansesu3 <juanseu3@hotmail.com>
2021-11-27 16:46:25 -0500

    Sixth commit estructure of home, InicioApp, NavBar, Login  and Registro

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,18 +32,7 @@ const Home = () => {
 
                     <p>Te sugerimos hacer alguno d eestos cursos con el fin de enrriquecer tu Curriculum Vitae</p>
                     <div><FontAwesomeIcon icon={faAngleDoubleDown} /></div>
-                    <Grid
-                        h="200px"
-                        templateRows="repeat(2, 1fr)"
-                        templateColumns="repeat(5, 1fr)"
-                        gap={4}
-                        >
-                        <GridItem rowSpan={2} colSpan={1} bg="tomato" />
-                        <GridItem colSpan={2} bg="papayawhip" />
-                        <GridItem colSpan={2} bg="papayawhip" />
-                        <GridItem colSpan={4} bg="tomato" />
-                    </Grid>
-                
+                   
                 
                  <NavBar/>
 
