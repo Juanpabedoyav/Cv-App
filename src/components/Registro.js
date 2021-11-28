5cb8be73914d671f79004b8bb1c4528bab53f270
Juan Pa Bedoya V <bedoyavascojuanpablo@gmail.com>
2021-11-27 20:12:30 -0500

    se realizan los estilos del componente Registro,js y se crea Registro.Styles.js

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -2,44 +2,56 @@ import React from "react";
 import {
   FormControl,
   FormLabel,
-  Form,
   Input,
   Button
 } from "@chakra-ui/react"
+// import {Form} from 'formik'
 import {Link as LinkReact } from 'react-router-dom'
-import { ImgLogo } from "../styles/InicioApp.styles";
+import {ImgRegistro, TitleRegistro, StyleRegistro } from "../styles/Registro.styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReply } from "@fortawesome/free-solid-svg-icons";
 
 const Registro = () => {
   
   return (
-    <div>
-      <h1>Registro</h1>
-      <div><ImgLogo src="https://res.cloudinary.com/dv08oqgvx/image/upload/v1637968857/mk3ocdc7zaenmvrmaskc.jpg" alt="" /></div>
-    
-          <FormControl id="first-name" isRequired>
-              <FormLabel>Telefono celular </FormLabel>
+    <StyleRegistro>
+        <LinkReact  className='back'to="/"><FontAwesomeIcon icon={faReply} /></LinkReact> 
+
+      <ImgRegistro>
+        <img src="https://res.cloudinary.com/dv08oqgvx/image/upload/v1637968857/mk3ocdc7zaenmvrmaskc.jpg" alt="logo" />
+
+        </ImgRegistro>
+        <TitleRegistro>Crear tu cuenta</TitleRegistro>
+<form className='formulario' >
+          <FormControl  id="first-name" isRequired>
+              <FormLabel className='label'>Telefono celular </FormLabel>
               <Input placeholder="Telefono celular " />
           </FormControl>
 
           <FormControl id="password" isRequired>
-              <FormLabel>Contraseña</FormLabel>
+              <FormLabel className='label'>Contraseña</FormLabel>
               <Input placeholder="Contraseña " />
           </FormControl>
 
           <FormControl id="password" isRequired>
-              <FormLabel>Confirmar contraseña</FormLabel>
+              <FormLabel className='label'>Confirmar contraseña</FormLabel>
               <Input placeholder="Confirmar contraseña " />
           </FormControl>
 
-          <Button colorScheme="teal" size="lg">
+          <Button className='botton-submit' colorScheme="teal" size="lg">
             Crear cuenta
           </Button>
-      <p>Si ya tines una cuenta <LinkReact to="/" className="styles-link-react">Ingresar </LinkReact >
+          </form>
+
+      <p>Si ya tines una cuenta  
+        <LinkReact 
+        to="/" 
+        className="styles-link-react">INGRESAR 
+        </LinkReact >
       </p>
 
 
-
-    </div>
+    </StyleRegistro>
   );
 };
 
