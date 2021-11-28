16b9476f23514958db67e2e653c723a21253affc
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-11-28 04:25:50 -0500

    Estilos del home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,41 +5,54 @@ import {
   faPlusCircle,
   faAngleDoubleDown,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link as LinkReact } from "react-router-dom";
+import { Link, Link as LinkReact } from "react-router-dom";
 import NavBar from "./NavBar";
+import { HomeContainer } from "../styles/Home.styles";
+import CursosHome from "./CursosHome";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <HomeContainer>
+      <h2 className="text-h2">Inicio</h2>
 
-      <HStack spacing={8}>
+      <div className="create">
         <LinkReact to="/formcv1">
           {" "}
-          <Box p={5} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
+          <Box
+            className="box"
+            shadow="md"
+            borderWidth="1px"
+            flex="1"
+            borderRadius="md"
+          >
             <Heading fontSize="xl">
-              <FontAwesomeIcon icon={faPlusCircle} />
+              <FontAwesomeIcon icon={faPlusCircle} className="icon" />
             </Heading>
-            <h4>Crear Curriculum </h4>
+            <h4 className="text-create">Crear Curriculum </h4>
           </Box>
         </LinkReact>
-      </HStack>
+      </div>
 
-      <h3>Recomendacione</h3>
-      <LinkReact to="/tutoriales">
-        <h3>Ver todo</h3>
-      </LinkReact>
+      <div className="other-container">
+        <h3 className="recomendaciones">Recomendaciones</h3>
+        <Link to="/tutoriales">
+          <h5>Ver todo</h5>
+        </Link>
+      </div>
 
-      <p>
+      <p className="text-p">
         Te sugerimos hacer alguno de estos cursos con el fin de enriquecer tu
         Curriculum Vitae
       </p>
-      <div>
+
+      <div className="arrow">
         <FontAwesomeIcon icon={faAngleDoubleDown} />
       </div>
 
+      <CursosHome />
+
       <NavBar />
-    </div>
+    </HomeContainer>
   );
 };
 
