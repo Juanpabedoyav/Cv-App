750832d5f29c2cacf479ae59b7fd2984b3823d37
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-12-02 16:13:20 -0500

    Agregando estilos al Login

---
+++ src/styles/Login.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+  background-color: #eef5f6;
+  height: 100vh;
+  color: var(--color-principal);
+  overflow: hidden;
+  .button {
+    background-color: var(--color-principal);
+    color: var(--color-blanco);
+  }
+`;
