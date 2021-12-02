f11a2450b1c222a87c50ed74f74c63ada1bec233
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-12-02 18:19:24 -0500

    Actualizando estados

--- src/styles/login.styles.js
+++ src/styles/login.styles.js
@@ -1,75 +1,35 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
-export const LoginContainer = styled.div`
-  background-color: #eef5f6;
-  height: 100vh;
-  color: var(--color-principal);
-  overflow: hidden;
-  .button {
-    background-color: var(--color-principal);
-    color: var(--color-blanco);
-  }
-  .button:hover {
-    background-color: var(--color-principal);
-    color: var(--color-blanco);
-  }
-  .input {
-    border: 1px solid var(--color-principal);
-  }
-  .pass {
-    border: 1px solid var(--color-principal);
-    margin-top: 15px;
-  }
-  .input::placeholder,
-  .pass::placeholder {
-    color: var(--color-principal);
-  }
-`;
-=======
 export const ContainerLogin = styled.div`
-    background-color: #eef5f6;
-    height: 710px;
-
-   
+  background-color: #eef5f6;
+  height: 710px;
 `;
 export const H1 = styled.h1`
-color: #629197;
-font-weight: bold;
-
-
+  color: #629197;
+  font-weight: bold;
 `;
 
 export const Label = styled.div`
-    width: 250px;
-    margin: auto;
-    padding-left: 63px;
-    padding-top: 20px;
-    color: #629197;
-    
-
+  width: 250px;
+  margin: auto;
+  padding-left: 63px;
+  padding-top: 20px;
+  color: #629197;
 `;
 export const Label1 = styled.div`
-    width: 250px;
-    margin: auto;
-    padding-left: 80px;
-    padding-top: 20px;
-    color: #629197;
-
+  width: 250px;
+  margin: auto;
+  padding-left: 80px;
+  padding-top: 20px;
+  color: #629197;
 `;
 export const IconUbi = styled.div`
-
-    position: relative;
-    top: 30px;
-    right: 140px;
-
+  position: relative;
+  top: 30px;
+  right: 140px;
 `;
 export const ForgotPassword = styled.div`
-    padding-top: 20px;
-    padding-bottom: 10px;
-    color: #629197;
-
-
-
+  padding-top: 20px;
+  padding-bottom: 10px;
+  color: #629197;
 `;
->>>>>>> 4ffe4eecf512fa83ff2730b332105997189c1c41
