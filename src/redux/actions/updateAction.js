ba55a8458580e75eaa937cc96b26db8fd575e597
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-12-08 22:43:51 -0500

    Función actualizar contraseña usuario

---
+++ src/redux/actions/updateAction.js
@@ -0,0 +1,21 @@
+import { db } from "../../firebase/firebase";
+import { doc, updateDoc, getDocs, collection } from "@firebase/firestore";
+
+export const updatePassword = (phone, password2) => {
+  return async (dispatch) => {
+    const getData = await getDocs(collection(db, "users"));
+    let docId = "";
+    getData.forEach((doc) => {
+      if (phone === doc.data().phone) docId = doc.id;
+    });
+
+    const docRef2 = doc(db, "users", docId);
+
+    await updateDoc(docRef2, {
+      password: password2,
+    })
+      .then((resp) => console.log(resp))
+
+      .catch((err) => console.log(err));
+  };
+};
