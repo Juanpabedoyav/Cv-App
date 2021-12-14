3029f02fcb1cb2b975acaac3de4c79247d3fbc08
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-12-14 12:42:31 -0500

    Agregando los tests

--- src/SetupTests.js
+++
@@ -1,6 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import {createSerializer} from 'enzyme-to-json';
-
-Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
