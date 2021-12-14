64801e21e76c07efe80c291b4da3050fe56b36a9
juansesu3 <juanseu3@hotmail.com>
2021-12-13 21:00:00 -0500

    prototype from plantilla cv 1 styles structure and some logic but I don't no why form1 and form2 is undefiened

--- src/components/PlantillaRenderCv1.js
+++ src/components/PlantillaRenderCv1.js
@@ -1,25 +1,77 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { ContainerPersonalData, ContainerPrincipal, ContainerProfesionalProfile, ContainerStudies, ContainerprofileImg, ContainerTitle} from '../styles/PlantillaRenderCv1';
 
 
 const PlantillaRenderCv1 = () => {
 
     const {form1, form2} = useSelector(state => state.pdfData)
+    // console.log(form1);
 
     return (
-        <div>
-            <h2>plantilla render</h2>
-            <h1>Hoja de vida </h1>
-            <div>
-                <img src="{}" />
-            </div>
-            <br/>
-            <hr/>
-            <p>Datos Personales</p>
+        <>
+
+        <ContainerPrincipal>
+              <h1 className='principal-title'>Hoja de vida </h1>
+                
+                    
+                        <ContainerprofileImg src="https://res.cloudinary.com/dv08oqgvx/image/upload/v1636073658/nmpby4t80lq3gvqjaosu.png" />
+                   
             
-        </div>
+                        {/* {form1.map((item) => ( */}
+                <ContainerPersonalData>
+                    
+                    <ContainerTitle><h2> DATOS PERSONALES </h2></ContainerTitle>
+                    <ul>
+                        <li><h3>Nombre: {"item.name"}</h3></li>
+                        <li><h3>Apellido: {"item.lastName"}</h3></li>
+                        <li><h3>E-mail: {"item.email"}</h3></li>
+                        <li><h3>Cargo: {"item.position"}</h3></li>
+                        <li><h3>Ubicacion: {"item.place"}</h3></li>
+                        <li><h3>Telefono: {"item.tags"}</h3></li>
+                    </ul> 
+                </ContainerPersonalData>
+                {/* ))} */}
+                {/* {form2.map((item2) => ( */}
+                <ContainerProfesionalProfile>
+                    <ContainerTitle><h2> PERFIL PROFESIONAL </h2></ContainerTitle>
+                    <ul>
+                        <li><h3>Cualidades: {"item2.tags1"}</h3></li>
+                        <li><h3>Motivaciones: {"item2.tags2"}</h3></li>
+                        <li><h3>Experincias laborales: {"item2.tags3"}</h3></li>
+                        <li><h3>Habilidades blandas: {"item2.tags4"}</h3></li>
+                        <li><h3>Idiomas: {"item2.tags5"}</h3></li>
+                       
+                    </ul>
+                    
+                    
+                    
+                    
+                </ContainerProfesionalProfile>
+                {/* ))} */}
+
+            </ContainerPrincipal>
+            
+        </>
     )
 }
 
 export default PlantillaRenderCv1
 
+// <CheckoutProduct
+                // key={item.name}
+                // id={item.lastName}
+                // title={item.email}
+                // image={item.position}
+                // price={item.place}
+                // rating={item.phone}
+                // />
+
+                      {/* {form1.map((item) => (
+                        <></>
+                    // console.log(item.name)
+
+
+
+                
+                    ))} */}
---
+++ src/styles/PlantillaRenderCv1.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+
+export const ContainerPrincipal = styled.div`
+
+
+height: 100vh  ;
+
+.principal-title{
+    padding-top: 1vh;
+}
+
+h1{
+    font-weight:800;   
+}
+
+h2{
+    text-align: center;
+    color: #fff;
+}
+
+h3{
+    text-align: start;
+    padding: 0 7vh 1vh; 
+
+}
+
+`
+export const ContainerTitle = styled.div`
+
+background-color: grey; 
+width: 90%;
+margin-left: auto;
+margin-right: auto;
+margin-bottom: 3vh;
+margin-top: 4vh;
+
+`
+export const ContainerprofileImg = styled.img`
+
+margin-left: auto;
+margin-right: auto;
+margin-top: 3vh;
+height: 20vh;
+width: 20vh;
+
+`
+
+export const ContainerPersonalData = styled.div`
+
+`
+export const ContainerProfesionalProfile = styled.div`
+
+`
+export const ContainerStudies = styled.div`
+
+`
